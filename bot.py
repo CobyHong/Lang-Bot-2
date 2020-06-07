2357b27ea3b53d85f6b2ff0577bdfdd83cacea8f
Coby Hong <cobyhong@gmail.com>
2020-06-06 23:36:20 -0700

    Update bot.py

--- bot.py
+++ bot.py
@@ -43,7 +43,7 @@ translator = Translator()
 @bot.event
 async def on_ready():
     print("Translator Bot is Online.")
-    return await bot.change_presence(activity=discord.Activity(type=discord.ActivityType.watching, name="'!' command prefix"))
+    return await bot.change_presence(activity=discord.Activity(type=discord.ActivityType.watching, name="Coby be cute :heart:"))
 
 
 #bot help command.
